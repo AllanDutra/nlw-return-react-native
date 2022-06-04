0e8ce4ed1bcf1d668b5285a6d7da6e0b10757bee
AllanDutra <allandutrafeitosa14@gmail.com>
2022-06-04 19:05:49 -0300

    feat: finishing the course

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -9,9 +9,24 @@ import {
 
 import { styles } from './styles';
 
-export function Option() {
+interface Props extends TouchableOpacityProps {
+    title: string;
+    image: ImageProps;
+}
+
+export function Option({ title, image, ...rest }: Props) {
     return (
-        <TouchableOpacity style={styles.container}>
+        <TouchableOpacity
+            style={styles.container}
+            {...rest}
+        >
+
+            <Image
+                source={image}
+                style={styles.image}
+            />
+
+            <Text style={styles.title}>{title}</Text>
 
         </TouchableOpacity>
     );
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 
@@ -8,13 +8,32 @@ import { theme } from '../../theme';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { Options } from '../Options';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+import { Form } from '../Form';
+import { Success } from '../Success';
+
+// pegar tipos das chaves do objeto feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
     const bottomSheetRef = useRef<BottomSheet>(null);
 
     function handleOpen() {
         bottomSheetRef.current?.expand();
+    }
+
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+        setFeedbackSent(false);
+    }
+
+    function handleFeedbackSent() {
+        setFeedbackSent(true);
     }
 
     return (
@@ -37,7 +56,25 @@ function Widget() {
                 backgroundStyle={styles.modal}
                 handleIndicatorStyle={styles.indicator}
             >
-                <Options />
+                {
+                    feedbackSent ?
+                        <Success
+                            onSendAnotherFeedback={handleRestartFeedback}
+                        />
+                        :
+                        <>
+                            {
+                                feedbackType ?
+                                    <Form
+                                        feedbackType={feedbackType}
+                                        onFeedbackCanceled={handleRestartFeedback}
+                                        onFeedbackSent={handleFeedbackSent}
+                                    />
+                                    :
+                                    <Options onFeedbackTypeChanged={setFeedbackType} />
+                            }
+                        </>
+                }
             </BottomSheet>
 
         </>
